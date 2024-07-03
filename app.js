a6e03a8d503f732d6789a77fd554d3d4cd82004d
ananijoel <ananijoellanatide@gmail.com>
2024-07-03 07:19:15 +0000

    feat(server): creation du serveur express

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("hello node")
+const express = require('express')
+
+const app = new express
+const port = 3000
+
+app.listen(port,()=> console.log(`Notre premiere application Node est demarée sur : http://localhost:${port}`))
