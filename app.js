61adc4991e5833d303a9721e3ef602b92fa92937
ananijoel <ananijoellanatide@gmail.com>
2024-07-03 07:22:02 +0000

    feat(endpoint): création du premier point de terminaison qui pointe vers la racine de l'application

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 
 const app = new express
 const port = 3000
-
+app.get('/',(req,res)=>res.send("hello node and express"))
 app.listen(port,()=> console.log(`Notre premiere application Node est demarée sur : http://localhost:${port}`))
