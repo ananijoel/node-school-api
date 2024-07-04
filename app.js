651433d1b60ff713d73243ff6cf6f7bde9cd813f
ananijoel <ananijoellanatide@gmail.com>
2024-07-04 05:43:29 +0000

    feat: creation d'un middleware personnel le middleware cree sert a afficher les logs des requettes entrantes dans l'API.

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ const express = require('express')
 
 const app = new express
 const port = 3000
+
+
+app.use((req,res,next) =>{
+    console.log(`URL : ${req.url}`)
+    next()
+})
+
 app.get('/',(req,res)=>res.send("hello node, express and nodemon"))
+app.get('/api/pokemons/:id/:name',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const name = req.params.name
+    res.send(`${id} - ${name}`)
+})
 app.listen(port,()=> console.log(`Notre premiere application Node est demarée sur : http://localhost:${port}`))
